43db2070611aee1a40de7d3a20e6c0c057cfe163
Oguzhan <oguzhanzny@gmail.com>
2023-12-06 17:55:34 +0300

    fix navbar conflict

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,7 +5,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <main className="min-h-screen bg-white dark:bg-nft-dark">
       <Navbar />
-      {children}
+      <div className="p-65">{children}</div>
       <Footer />
     </main>
   );
